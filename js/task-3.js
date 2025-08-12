e21bae8ca2bf988ac3bf0a80219dff73e65bd070
Nazar_O <nazar.orlovskuy49@gmail.com>
2025-08-11 22:34:30 -0500

    оновлено

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
-const sortByDescendingFriendCount = (users) =>
-  [...users].sort((a, b) => b.friends.length - a.friends.length);
+const getUsersSortedByFriendsCount = (users) => 
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 
 console.log(
